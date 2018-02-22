7df1f8f7d2094f972f607a77a9d9c0a0e47f012c
Bruce Harris <bruceharris91@gmail.com>
2018-02-22 09:54:53 -0600

    added navigation and details page

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
-import HomeScreen from './screens/HomeScreen' 
+import { StackNavigator } from 'react-navigation';
+
+import HomeScreen from './screens/HomeScreen';
+import ProjectDetailScreen from './screens/ProjectDetailScreen'; 
+
+const RootNavigator = StackNavigator({
+  Home: { screen: HomeScreen },
+  ProjectDetail: { screen: ProjectDetailScreen}
+}, { 
+  initialRouteName: 'Home'
+});
 
 export default class App extends Component {
   render() {
     return (
-      <HomeScreen />
-    )
+      <RootNavigator />
+    );
   }
 }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 import { ScrollView, Text, StyleSheet } from 'react-native'
+import ProjectCard from '../components/ProjectCard'
 
 export default class HomeScreen extends Component {
+
+    static navigationOptions = {
+        title: 'Covalence Projects'
+    }
 
     constructor(props) {
         super(props)
@@ -11,23 +16,29 @@ export default class HomeScreen extends Component {
     }
 
     componentDidMount() {
-            this.setState({
-                projects:[
-                    {name: 'Test Project 1', description: 'coolio proj'},
-                    {name: 'Test Project 2', description: 'coolio proj'},
-                    {name: 'Test Project 3', description: 'coolio proj'},
-                    {name: 'Test Project 4', description: 'coolio proj'}
+        this.setState({
+            projects: [
+                { name: 'Test Project 1', description: 'coolio proj' },
+                { name: 'Test Project 2', description: 'coolio proj' },
+                { name: 'Test Project 3', description: 'coolio proj' },
+                { name: 'Test Project 4', description: 'coolio proj' }
 
-                ]
-            })
+            ]
+        })
+    }
+
+    navigate(project) {
+        this.props.navigation.navigate('ProjectDetail', { project: project });
     }
 
     render() {
         return (
             <ScrollView style={styles.container}>
-                { this.state.projects.map((project, index) => {
-                    return <Text key={project.index}>{ project.name }</Text>
-                }) }
+                {this.state.projects.map((project, index) => {
+                    return <ProjectCard key={index} project={project} 
+                                Navigate={ () => {this.navigate(project)} } 
+                            />
+                })}
             </ScrollView>
         )
     }
@@ -36,7 +47,6 @@ export default class HomeScreen extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        paddingTop: 30,
         paddingLeft: 10,
         paddingRight: 10
     }
